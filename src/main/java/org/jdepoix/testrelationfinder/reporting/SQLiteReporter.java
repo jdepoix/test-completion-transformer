ad20164fa38fa9b764b11d091d4a1da05522e4dc
Jonas Depoix <jdepoix@seibert-media.net>
2020-08-07 17:19:39 +0200

    added sqlite reporting

---
+++ src/main/java/org/jdepoix/testrelationfinder/reporting/SQLiteReporter.java
@@ -0,0 +1,74 @@
+package org.jdepoix.testrelationfinder.reporting;
+
+import org.jdepoix.testrelationfinder.relation.ResolvedTestRelation;
+
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class SQLiteReporter {
+    private static final String INSERT_TEST_RELATION_SQL =
+        "INSERT INTO test_relations VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+    private static final int BATCH_SIZE = 1000;
+
+    private final Path dbFile;
+
+    public SQLiteReporter(Path dbFile) {
+        this.dbFile = dbFile;
+    }
+
+    public void reportResults(String repoName, List<ResolvedTestRelation> testRelations)
+        throws SQLException
+    {
+        try (
+            final Connection connection = DriverManager.getConnection(
+                String.format("jdbc:sqlite:%s", this.dbFile.toString())
+            )
+        ) {
+            connection.setAutoCommit(false);
+            insertTestRelations(connection, repoName, testRelations);
+            connection.commit();
+        }
+    }
+
+    private void insertTestRelations(Connection connection, String repoName, List<ResolvedTestRelation> testRelations)
+        throws SQLException
+    {
+        final PreparedStatement preparedStatement = connection.prepareStatement(INSERT_TEST_RELATION_SQL);
+        final int size = testRelations.size();
+        int fullBatches = size / BATCH_SIZE;
+
+        for (int i = 0; i < fullBatches; i++) {
+            for (int j = i * BATCH_SIZE; j < (i + 1) * BATCH_SIZE; j++) {
+                this.setupPreparedStatement(preparedStatement, repoName, testRelations.get(j));
+            }
+            preparedStatement.executeBatch();
+        }
+        for (int i = fullBatches * BATCH_SIZE; i < fullBatches * BATCH_SIZE + size % BATCH_SIZE; i++) {
+            this.setupPreparedStatement(preparedStatement, repoName, testRelations.get(i));
+        }
+        preparedStatement.executeBatch();
+    }
+
+    private void setupPreparedStatement(
+        PreparedStatement preparedStatement,
+        String repoName,
+        ResolvedTestRelation testRelation
+    ) throws SQLException {
+        preparedStatement.setString(1, repoName);
+        preparedStatement.setString(2, testRelation.getTestMethodPackageName());
+        preparedStatement.setString(3, testRelation.getTestMethodClassName());
+        preparedStatement.setString(4, testRelation.getTestMethodName());
+        preparedStatement.setString(5, testRelation.getTestPath().toString());
+        preparedStatement.setString(6, testRelation.getRelatedMethodPackageName().orElse(null));
+        preparedStatement.setString(7, testRelation.getRelatedMethodClassName().orElse(null));
+        preparedStatement.setString(8, testRelation.getRelatedMethodName().orElse(null));
+        preparedStatement.setString(9, testRelation.getRelatedMethodPath().map(Path::toString).orElse(null));
+        preparedStatement.setString(10, testRelation.getResolutionStatus().toString());
+        preparedStatement.setString(11, testRelation.getRelationType().toString());
+        preparedStatement.addBatch();
+    }
+}
