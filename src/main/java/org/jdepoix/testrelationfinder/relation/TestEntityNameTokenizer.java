eb2fe69093bc1845a17041652d1dd25708af1fea
Jonas Depoix <jdepoix@seibert-media.net>
2020-08-17 15:58:48 +0200

    added snake case support
    Former-commit-id: 78d162386aa290322f5bcf97fa60c5ffc09cc7e4

--- src/main/java/org/jdepoix/testrelationfinder/relation/TestEntityNameTokenizer.java
+++ src/main/java/org/jdepoix/testrelationfinder/relation/TestEntityNameTokenizer.java
@@ -1,13 +1,27 @@
 package org.jdepoix.testrelationfinder.relation;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 class TestEntityNameTokenizer {
+    private static final Pattern MATCH_LOWERCASE = Pattern.compile("[^A-Z]+");
+    private static final Pattern MATCH_SURROUNDED_UNDERSCORES = Pattern.compile(".+_.+");
+
     String[] tokenize(String name) {
         return Arrays
-            .stream(name.split("(?=\\p{Lu})"))
+            .stream(this.splitMethodName(name))
             .map(String::toLowerCase)
-            .filter(s -> !s.equals("test"))
+            .filter(s -> !(s.equals("") || s.equals("test")))
             .toArray(String[]::new);
+    }
+
+    private String[] splitMethodName(String name) {
+        if (name.length() == 0) {
+            return new String[]{};
+        }
+        if (MATCH_LOWERCASE.matcher(name).matches() && MATCH_SURROUNDED_UNDERSCORES.matcher(name).matches()) {
+            return name.split("_");
+        }
+        return name.split("(?=\\p{Lu})");
     }
 }
