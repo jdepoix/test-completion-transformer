81f2234b1dbbe9814eb57182043a677d81ba7a65
Jonas Depoix <jdepoix@seibert-media.net>
2020-10-17 18:21:14 +0200

    added shuffle to dataloaders

--- src/data.py
+++ src/data.py
@@ -140,6 +140,7 @@ class GwtDataModule(LightningDataModule):
     def train_dataloader(self):
         return DataLoader(
             self._train_dataset,
+            shuffle=True,
             batch_size=self._batch_size,
             collate_fn=self._batch_padder.create_batch,
             num_workers=self._num_worker,
@@ -148,6 +149,7 @@ class GwtDataModule(LightningDataModule):
     def val_dataloader(self):
         return DataLoader(
             self._validation_dataset,
+            shuffle=True,
             batch_size=self._batch_size,
             collate_fn=self._batch_padder.create_batch,
             num_workers=self._num_worker,
@@ -156,6 +158,7 @@ class GwtDataModule(LightningDataModule):
     def test_dataloader(self):
         return DataLoader(
             self._test_dataset,
+            shuffle=True,
             batch_size=self._batch_size,
             collate_fn=self._batch_padder.create_batch,
             num_workers=self._num_worker,
